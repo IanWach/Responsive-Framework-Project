e4fefaca20a2dec1859d0a64b1c8d2458ee6d70e
Ian Wach <iankaveli0@gmail.com>
2022-06-18 22:52:13 +0300

    Editing the Framework files

--- framework/webresp.js
+++ framework/webresp.js
@@ -2,12 +2,41 @@ var windowSize = window.innerWidth;
 
 
 
-    if (windowSize >= 769) {
-        console.log('Greater than 768');
-        document.getElementById('here').addClass = 'full-page';
-}
+//     if (windowSize >= 769) {
+//         console.log('Greater than 768');
+//         document.getElementById('here').addClass = 'full-page';
+// }
 
-    else  {
-        console.log('Less than 768');
-        document.getElementById('here').removeClass = 'full-page';
+//     else  {
+//         console.log('Less than 768');
+//         document.getElementById('here').removeClass = 'full-page';
+// }
+
+var btn = document.getElementById('webresp-hide1');
+var nav = document.getElementById('nav-lists');
+var hideIcon = document.getElementsByClassName('webresp-hide')
+
+    if(windowSize <= 767){
+        hideIcon[0].style.display = "block";
+        nav.addClass = 'mbresp-navbar';
+        
+    }
+    else{
+        alert('This Is It ' + windowSize)
+    }
+
+
+function showNavbar(){
+    if (windowSize <= 480) {
+        nav.addClass = 'mbresp-navbar';
+        nav.style.display = 'block';
+    }
+    if(windowSize<= 767){
+        
+        nav.addClass = 'tbresp-navbar';
+        nav.addClass = 'tbresp-logo'
+    }
+    else{
+        console.log('The Screen Size is :', windowSize);
+    }
 }
