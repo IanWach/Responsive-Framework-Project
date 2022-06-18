609703b56ba070ff9b5f62b3759891897b113ac4
Ian Wach <iankaveli0@gmail.com>
2022-06-18 20:33:33 +0300

    Addeded some HTML, CSS & JS and Images

---
+++ framework/webresp.js
@@ -0,0 +1,13 @@
+var windowSize = window.innerWidth;
+
+
+
+    if (windowSize >= 769) {
+        console.log('Greater than 768');
+        document.getElementById('here').addClass = 'full-page';
+}
+
+    else  {
+        console.log('Less than 768');
+        document.getElementById('here').removeClass = 'full-page';
+}
