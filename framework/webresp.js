dbecc5a2e6b7cff4e592e86e475f7c54c5972f55
Ian Wach <iankaveli0@gmail.com>
2022-06-26 23:34:08 +0300

    Made Some Changes on responsiveness

--- framework/webresp.js
+++ framework/webresp.js
@@ -13,12 +13,15 @@ var windowSize = window.innerWidth;
 // }
 
 var btn = document.getElementById('webresp-hide1');
-var nav = document.getElementById('nav-lists');
+var nav = document.getElementById('navbar');
+var navList = document.getElementById('nav-list');
+var navitem2 = document.getElementById('navbaritem2');
 var hideIcon = document.getElementsByClassName('webresp-hide')
 
     if(windowSize <= 767){
         hideIcon[0].style.display = "block";
-        nav.addClass = 'mbresp-navbar';
+        nav.classList.add("mbresp-navbar");
+        navitem2.classList.add("mbresp-navbaritem2");
         
     }
     else{
@@ -30,11 +33,13 @@ function showNavbar(){
     if (windowSize <= 480) {
         nav.addClass = 'mbresp-navbar';
         nav.style.display = 'block';
+        navList.classList.add = 'mbresp-navlist';
+        
     }
     if(windowSize<= 767){
         
-        nav.addClass = 'tbresp-navbar';
-        nav.addClass = 'tbresp-logo'
+        navList.classList.add = 'mbresp-navlist';
+        nav.classList.add = 'tbresp-logo'
     }
     else{
         console.log('The Screen Size is :', windowSize);
