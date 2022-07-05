59b3a65c83a97efa20521504a813999d90e1a565
Ian Wach <iankaveli0@gmail.com>
2022-07-05 16:49:11 +0300

    Additional Edits before the React

--- framework/webresp.js
+++ framework/webresp.js
@@ -3,12 +3,16 @@
 // $(windowSize).resize(windowSize = $(windowSize).innerWidth())
 function changescreenElement(){
 var windowSize = window.innerWidth;   
-console.log("We have" +windowSize);
+console.log("We have " +windowSize);
 return windowSize;
 }
-window.onresize = function(){
+window.onresize = () => {
+
     changescreenElement()
+    var windowSize = window.innerWidth;
     }
+
+
 var windowSize = changescreenElement()
 var btn = document.getElementById('webresp-hide1');
 var nav = document.getElementById('navbar');
@@ -27,19 +31,21 @@ This code entails A Conditional Statement to detect the window Size
 and then it adds Class name to the declared variables above
 */
     if(windowSize <= 480){
-        hideIcon[0].style.display = "block";
+        hideIcon[0].style.display = "block !important";
         nav.classList.add("mbresp-navbar");
         navitem2.classList.add("mbresp-navbaritem2");
         
     }
-   else if(windowSize > 480 && windowSize<= 767){
+    else if(windowSize > 480 && windowSize<= 767){
         hideIcon[0].style.display = "block";
+        nav.style.display = "none";
         nav.classList.add("tbresp-navbar");
         navitem2.classList.add("tbresp-navbaritem2");
     }
     else{
         alert('This Is It ' + windowSize)
     }
+//Function to reload screen when there is a Resize
 
     function changeScreenElement1(){
         var screenSize = window.innerWidth;
@@ -48,7 +54,7 @@ and then it adds Class name to the declared variables above
         window.onresize = function(){
             changeScreenElement1()
             }
-
+//Function for an Event Listener when the Menu Bar in the Navbar is clicked.
 function showNavbar(screenSize){
         
     var screenSize = changeScreenElement1()
@@ -71,4 +77,9 @@ function showNavbar(screenSize){
         alert('Screen Size is : ' +screenSize);
     }
    
+}
+
+window.onresize = () => {
+    location.reload();
+    
 }
