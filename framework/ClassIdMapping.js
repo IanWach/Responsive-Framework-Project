864c2fadcffc36a58b73f35718a76f47feca13b2
Ian Wach <iankaveli0@gmail.com>
2022-07-08 00:25:00 +0300

    Grateful to God I Cracked It Out

---
+++ framework/ClassIdMapping.js
@@ -0,0 +1,33 @@
+//Importing Datasets
+import {responsiveAttributes} from './classIdDataset.js';
+// $(windowSize).resize(windowSize = $(windowSize).innerWidth())
+function changescreenElement(){
+var windowSize = window.innerWidth;   
+console.log("We have " +windowSize);
+return windowSize;
+}
+window.onresize = () => {
+
+    changescreenElement()
+    var windowSize = window.innerWidth;
+    }
+//Access the Website Attributes
+responsiveAttributes.map((Item, index) =>{
+
+    var variableName = document.getElementsByClassName(Item.previousClass);
+    console.log(variableName);
+    var windowSize = changescreenElement()
+    if(windowSize <= 480){
+            for (let i = 0; i < variableName.length; i++) {
+                variableName.item(i).classList.add(Item.mobileClass);
+                }
+        }
+    else if(windowSize > 480 && windowSize<= 767){  
+        for (let i = 0; i < variableName.length; i++) {
+            variableName.item(i).classList.add(Item.tabletClass);
+        }
+    }
+    else{
+        
+    }
+  });
