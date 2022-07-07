c2d1bda0f6573879c917b8a5b16751ed4a415348
Ian Wach <iankaveli0@gmail.com>
2022-07-08 00:26:14 +0300

    Editting the Name

--- framework/class&IdMap.js
+++
@@ -1,9 +0,0 @@
-//Importing Datasets
-import {responsiveAttributes} from './classIdDataset.js';
-
-//Access the Website Attributes
-responsiveAttributes.map((Item, index) =>{
-
-        var variableName = document.getElementById(Item.variableName);    
-        console.log(variableName);
-      })
