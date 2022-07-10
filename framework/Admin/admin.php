db898d95c5dfd37cf3c42de6fd4472d0a736acb7
Ian Wach <iankaveli0@gmail.com>
2022-07-11 00:52:35 +0300

    Changes to the Admin Files

--- framework/Admin/admin.php
+++ framework/Admin/admin.php
@@ -1,45 +1,46 @@
 <?php 
 
  $error =NULL;
+  //Code to check if the submit form is clicked
   if (isset ($_POST['submit'])){
 
-
     //Get From data
-     $user = $_POST['username'];
-     $mail  = $_POST['email'];
-     $pass = $_POST['password'];
-     $confirm = $_POST['confirm'];
-
-     
-    
+     $PrevClass = $_POST['prev-class'];
+     $AddId  = $_POST['addId'];
+     $TabletClass = $_POST['tablet-c'];
+     $MobileClass = $_POST['mobile-c'];
+    //Coditions to ensure no empty Inputs and Short attributes
  
-     if(strlen ($user) < 5 ){
-       $error = " Your Username must be at least 5 characters";
+     if(strlen ($PrevClass) < 3 ){
+       $error = " Your Class Name must be at least 5 characters";
        echo $error;
        }
-       elseif($pass!=$confirm){
-         $error ="The passwords do not match";
+       elseif( strlen($AddId) < 5){
+         $error ="The ID Name is Too Short";
          echo $error;
+       }
+       elseif(strlen($TabletClass||$MobileClass)<10){
+        $error = "The Mobile Class or Tablet Class is to Short (at least 10 characters";
        }
        else{
          //Form is Valid
  
          //Connect to Database
-         $db_name="peep";
+         $db_name="IntraResp";
  
         $mysqli =NEW mysqli ('localhost','root','',$db_name) or die ("error:".mysqli_error($mysqli));
  
          //Clean Data
-         $user = $mysqli->real_escape_string($user);
-         $mail = $mysqli->real_escape_string($mail);
-         $pass = $mysqli->real_escape_string($pass);
-         $confirm = $mysqli->real_escape_string($confirm);
+         $PrevClass = $mysqli->real_escape_string($PrevClass);
+         $AddId = $mysqli->real_escape_string($AddId);
+         $TabletClass = $mysqli->real_escape_string($TabletClass);
+         $MobileClass = $mysqli->real_escape_string($MobileClass);
          
    
  
          //generate key
-         $key = md5( time ().$user);
-         $insert=mysqli_query($mysqli,"insert into login  values ('','$user','$mail','$pass','$key') ") or die (mysqli_error($mysqli));
+         $key = md5( time ().$AddId);
+         $insert=mysqli_query($mysqli,"insert into login  values ('','$PrevClass','$AddId','$TabletClass','$MobileClass','$key') ") or die (mysqli_error($mysqli));
         
          $success=mysqli_query($mysqli,$insert);
          if($insert){
